06a7a26a65c3a7c7bc4eb2ebb6e4119e1c756f04
Raul Reyes <chinorrm@hotmail.com>
2026-04-12 17:59:24 -0600

    fix en middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,21 +1,42 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
 
 const isPublicRoute = createRouteMatcher([
   "/",
   "/sign-in(.*)",
   "/sign-up(.*)",
-  "/onboarding(.*)",
-  "/select-org(.*)",
   "/api/webhooks(.*)",
 ]);
 
+const isOrgSetupRoute = createRouteMatcher([
+  "/onboarding(.*)",
+  "/select-org(.*)",
+]);
+
 export default clerkMiddleware((auth, request) => {
-  if (!isPublicRoute(request)) {
+  if (isPublicRoute(request)) return NextResponse.next();
+
+  const { userId, orgId } = auth();
+
+  if (!userId) {
     auth().protect();
+    return;
   }
+
+  if (orgId && isOrgSetupRoute(request)) {
+    return NextResponse.redirect(new URL("/dashboard", request.url));
+  }
+
+  if (!orgId && !isOrgSetupRoute(request)) {
+    return NextResponse.redirect(new URL("/select-org", request.url));
+  }
+
+  return NextResponse.next();
 });
 
 export const config = {
-  matcher: ["/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)", "/(api|trpc)(.*)"],
+  matcher: [
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    "/(api|trpc)(.*)",
+  ],
 };
-
